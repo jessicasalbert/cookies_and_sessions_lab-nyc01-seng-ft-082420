b11537a76194252e8dd311763af36aab74fcf317
jessicasalbert <jessicarsalbert@gmail.com>
2020-09-23 09:07:23 -0400

    4/5 tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: "products#index", as: "products"
-  post '/add', to: "products#add", as: "add"
+  post '/', to: "products#add", as: "add"
 end
